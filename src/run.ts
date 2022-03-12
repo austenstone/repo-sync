51ab42a966e94741390b1871ed94a2dd93e43028
Austen Stone <austenstone@github.com>
2022-03-12 17:22:12 -0500

    test

--- src/run.ts
+++ src/run.ts
@@ -3,10 +3,18 @@ import * as core from '@actions/core';
 import * as glob from '@actions/glob';
 
 const run = async (): Promise<void> => {
-  const pattern = core.getInput('pattern');
-  const globber = await glob.create(pattern)
-  const files = await globber.glob()
-  console.log(files);
+  const patternInput = core.getInput('pattern');
+  try {
+    const pattern = JSON.parse(patternInput);
+    if (typeof pattern !== 'string' && !Array.isArray(pattern)) {
+      throw Error(`Invalid input [pattern]: ${patternInput}`)
+    }
+    const globber = await glob.create([...pattern].join('\n'))
+    const files = await globber.glob()
+    console.log(files);
+  } catch (error) {
+    core.error(error instanceof Error ? error.message : JSON.stringify(error))
+  }
 };
 
 export default run;
